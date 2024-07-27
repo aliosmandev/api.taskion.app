ceebb66a3adb80492dc60674d57098c55b6bf8ae
Ali Osman <dlsmnosman@gmail.com>
2024-07-27 15:40:18 +0300

    feat: update env loader

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"path/filepath"
 	"taskmanager/cmd/application"
 
 	"github.com/joho/godotenv"
@@ -15,37 +14,21 @@ func main() {
 }
 
 func LoadENV() {
-	// Ortam değişkenini oku
 	env := os.Getenv("APP_ENV")
 	if env == "" {
-		env = "development" // Default olarak development
+		env = "development"
 	}
 
 	var envPath string
 	var err error
 
 	if env == "production" {
-		executable, err := os.Executable()
-		if err != nil {
-			log.Fatalf("Error getting executable path: %s", err)
-		}
-		exPath := filepath.Join(filepath.Dir(executable), "../api")
-		err = os.Chdir(exPath)
-		if err != nil {
-			log.Fatalf("Error changing working directory: %s", err)
-		}
-		cwd, err := os.Getwd()
-		if err != nil {
-			log.Fatalf("Error getting current working directory: %s", err)
-		}
-		envPath = filepath.Join(cwd, ".env")
 	} else {
-		envPath = ".env"
+		err = godotenv.Load(".env")
+		if err != nil {
+			log.Fatalf("Error loading .env file from %s: %s", envPath, err)
+		}
+		log.Printf("Loaded .env file from %s", envPath)
 	}
 
-	err = godotenv.Load(envPath)
-	if err != nil {
-		log.Fatalf("Error loading .env file from %s: %s", envPath, err)
-	}
-	log.Printf("Loaded .env file from %s", envPath)
 }
