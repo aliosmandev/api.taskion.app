210dd9d85bab6d0ee65dc5391f977ff47ee7aed5
Ali Osman <dlsmnosman@gmail.com>
2024-07-29 00:17:33 +0300

    feat: add blocks delete api

--- modules/blocks/router.go
+++ modules/blocks/router.go
@@ -8,4 +8,5 @@ func InitRouter(router fiber.Router) {
 	router.Get("/:pageId", getBlocks)
 	router.Post("/create/:pageId", createBlock)
 	router.Put("/update/:blockId", updateBlock)
+	router.Delete("/delete/:blockId", deleteBlock)
 }
--- modules/blocks/service.go
+++ modules/blocks/service.go
@@ -266,3 +266,43 @@ func updateBlock(c *fiber.Ctx) error {
 
 	return c.JSON(responseBody)
 }
+
+func deleteBlock(c *fiber.Ctx) error {
+	var blockId string = c.Params("blockId")
+
+	var deleteUrl string = "https://api.notion.com/v1/blocks/" + blockId
+
+	r, err := http.NewRequest("DELETE", deleteUrl, nil)
+	if err != nil {
+		return c.JSON(500, "error")
+	}
+
+	Authorization := c.GetReqHeaders()["Authorization"]
+	accessToken := Authorization[len(Authorization)-1][7:]
+
+	r.Header.Set("Content-Type", "application/json")
+	r.Header.Set("Authorization", "Bearer "+accessToken)
+	r.Header.Set("Notion-Version", "2022-02-22")
+
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		return c.JSON(500, "error")
+	}
+	defer res.Body.Close()
+
+	bodyBytes, _ := io.ReadAll(res.Body)
+
+	if res.StatusCode != http.StatusOK {
+		return c.Status(res.StatusCode).JSON(string(bodyBytes))
+	}
+
+	var responseBody map[string]interface{}
+
+	err = json.Unmarshal(bodyBytes, &responseBody)
+	if err != nil {
+		return c.JSON(500, "error")
+	}
+
+	return c.JSON(responseBody)
+}
